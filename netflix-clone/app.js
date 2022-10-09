40ed377aa002bc1b7ee3104acbcfc66bfbc9234f
https://github.com/juayuohcarineneng19/ <juayuoh@gmail.com>
2022-10-09 13:33:36 -0700

    js on valid email

--- netflix-clone/app.js
+++ netflix-clone/app.js
@@ -10,7 +10,33 @@ const FOCUS_IN = 'focusin'
 const validateEmail = (email) =>
 /^[A-Z0-9.]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)
 
-const handleFocus = e
+const handleFocus = (e) => {
+    const{
+        target:{ value},
+        type,
+    } = e
+    if (value) return true
+    if (type == FOCUS_IN){
+        membershipInputContainer.classList.add(ON_FOCUS)
+        return true
+    }
+    membershipInputContainer.classList.remove(ON_FOCUS)
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    const { value } = membershipInput
+    const validEmail = validateEmail(value)
+    if (validEmail) {
+        membershipInputMessage.innerText = 'Thank you for understanding'
+        membershipInput.value = ''
+        membershipInputContainer.classList.remove(ON_FOCUS)
+
+    } else{
+        membershipInputMessage.innerText = 'Enter a valid Email'
+    }
+}
 
 membershipInput.addEventListener(FOCUS_IN, handleFocus)
-membershipInput.addEventListener(focusout, handleFocus)
+membershipInput.addEventListener('focusout', handleFocus)
+membershipInput.addEventListener(submit, handleSubmit)
